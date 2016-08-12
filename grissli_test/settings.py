88849eda32d84a143e8e2e2e4ad4b3c13b82f933
andrewshkovskii <andrewshkovskii@gmail.com>
2016-08-12 07:33:24 +0300

    Скачанные изображения теперь храняться в /tmp/

--- grissli_test/settings.py
+++ grissli_test/settings.py
@@ -12,6 +12,6 @@ PROJECT_DIR = os.path.dirname(__file__)
 
 TEMPLATES_DIR = os.path.join(PROJECT_DIR, 'templates')
 
-IMAGE_DIR = os.path.join(PROJECT_DIR, 'images')
+IMAGE_DIR = os.path.join('/tmp/')
 
 STATIC_DIR = os.path.join(PROJECT_DIR, 'static')
